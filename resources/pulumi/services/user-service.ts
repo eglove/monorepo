ca26ab4179c59eb14c9b9fdce0a17cb113549ab7
Ethan Glover <hello@ethang.email>
2024-01-01 13:04:26 -0600

    Refactor path usage in 'user-service.ts'
    This commit refactors the 'user-service.ts' code to use a defined projectRoot variable for file paths. This improves readability by removing repetitive, hard-coded paths. The 'dockerfile' and 'context' fields in the docker.Image construction now utilize this variable.

--- resources/pulumi/services/user-service.ts
+++ resources/pulumi/services/user-service.ts
@@ -6,17 +6,16 @@ import * as k8s from '@pulumi/kubernetes';
 export const userServiceName = 'user-service';
 
 export function userService() {
-  const packageJson = readFileSync(
-    '../../apps/user-service/package.json',
-    'utf8',
-  );
+  const projectRoot = '../../apps/user-service';
+
+  const packageJson = readFileSync(`${projectRoot}/package.json`, 'utf8');
   const parsed = JSON.parse(packageJson) as { version: string };
   const { version } = parsed;
 
   const userServiceImage = new docker.Image(`${userServiceName}-image`, {
     build: {
-      context: '../../apps/user-service',
-      dockerfile: '../../apps/user-service/Dockerfile',
+      context: projectRoot,
+      dockerfile: `${projectRoot}/Dockerfile`,
       platform: 'linux/amd64',
     },
     imageName: `docker.io/eglove/user-service:${version}`,
