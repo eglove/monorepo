e62238fb6277f710cb10a902e3a04116208c0640
Ethan Glover <hello@ethang.email>
2024-01-05 20:32:50 -0600

    Remove dotenv config from index.ts

--- resources/pulumi/index.ts
+++ resources/pulumi/index.ts
@@ -1,9 +1,5 @@
-import { config } from 'dotenv';
-
 import { imageOptimization } from './services/image-optimization/image-optimization';
 import { userService } from './services/user-service/user-service';
-
-config();
 
 userService();
 imageOptimization();
