5ea97a442dd8f5e3a0b887b176bb0f0fd3664db2
Ethan Glover <hello@ethang.email>
2024-01-18 17:18:34 -0600

    Update tsupOptions in 'toolbelt' build script
    Added 'minify' option and updated 'format' option in 'toolbelt' library's build script. These changes introduce minification for the built library and set the output module formats to both

--- libraries/toolbelt/build.mjs
+++ libraries/toolbelt/build.mjs
@@ -1,12 +1,13 @@
 import { projectBuilder } from '@ethang/project-builder/project-builder.js';
 
-// pnpm tsup src/* --outDir dist --format esm --format cjs
 await projectBuilder('toolbelt', 'main', {
   preVersionBumpScripts: ['UPDATE', 'PRUNE'],
   postVersionBumpScripts: ['DEDUPE', 'LINT'],
   publishDirectory: 'dist',
   isLibrary: true,
   tsupOptions: {
+    format: ['cjs', 'esm'],
+    minify: true,
     outDir: 'dist',
     entry: ['src/*']
   }
