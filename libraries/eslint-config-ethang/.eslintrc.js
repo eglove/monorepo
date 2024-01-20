c024ff56c291b97c5f9baabfad0d0151cbaee8f7
Ethan Glover <hello@ethang.email>
2024-01-20 11:20:09 -0600

    eslint update

--- libraries/eslint-config-ethang/.eslintrc.js
+++ libraries/eslint-config-ethang/.eslintrc.js
@@ -42,7 +42,36 @@ module.exports = {
     'node_modules',
   ],
   'rules': {
+    camelcase: 'off',
+    '@typescript-eslint/ban-types': [
+      'error',
+      {
+        types: {
+          null: false,
+        },
+      },
+    ],
+    '@typescript-eslint/consistent-type-definitions': ['error', 'type'],
+    '@typescript-eslint/consistent-type-imports': 'error',
+    '@typescript-eslint/explicit-member-accessibility': 'error',
+    '@typescript-eslint/naming-convention': 'off',
+    '@typescript-eslint/no-explicit-any': 'error',
+    '@typescript-eslint/no-floating-promises': 'error',
+    '@typescript-eslint/no-import-type-side-effects': 'error',
+    '@typescript-eslint/strict-boolean-expressions': 'error',
     'arrow-body-style': ['error', 'always'],
+    'capitalized-comments': 'off',
+    'max-params': 'error',
+    'new-cap': 'off',
+    'no-console': [
+      'error',
+      {
+        allow: ['debug', 'info', 'warn', 'error', 'table'],
+      },
+    ],
+    'no-empty-static-block': 'off',
+    'no-new-native-nonconstructor': 'off',
+    'no-undef': 'off',
     'prettier/prettier': [
       'error',
       {
@@ -53,7 +82,17 @@ module.exports = {
         'trailingComma': 'all',
       },
     ],
+    'react/no-array-index-key': 'error',
+    'react/no-multi-comp': 'error',
+    'react/no-unknown-property': 'warn',
+    'react/react-in-jsx-scope': 'off',
+    'react/require-default-props': 'error',
+    'simple-import-sort/exports': 'error',
+    'simple-import-sort/imports': 'error',
+    'sort-keys-fix/sort-keys-fix': 'error',
+    'unicorn/no-keyword-prefix': 'off',
     'unicorn/no-null': 'off',
+    'unicorn/prefer-set-has': 'error',
     'unused-imports/no-unused-imports': 'error',
     'unused-imports/no-unused-vars': [
       'warn',
