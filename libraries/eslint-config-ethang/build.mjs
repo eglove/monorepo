fa36ca48ae611061aea10ac848d7956ae1ff5636
Ethan Glover <hello@ethang.email>
2024-02-15 10:36:25 -0600

    remove eslint config

--- libraries/eslint-config-ethang/build.mjs
+++
@@ -1,8 +0,0 @@
-import { projectBuilder } from '@ethang/project-builder/project-builder.js';
-
-await projectBuilder('eslint-config-ethang', 'main', {
-  isLibrary: true,
-  preVersionBumpScripts: ['UPDATE', 'PRUNE'],
-  postVersionBumpScripts: ['DEDUPE', 'LINT'],
-  ignorePeerDependencies: ['@rushstack/eslint-patch'],
-})
