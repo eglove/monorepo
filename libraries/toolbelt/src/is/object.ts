511f0bc3eaf5f06572572e07f0a17e37e794a299
Ethan Glover <hello@ethang.email>
2024-02-15 12:29:01 -0600

    libraries remove

--- libraries/toolbelt/src/is/object.ts
+++
@@ -1,4 +0,0 @@
-export function isObject(value: unknown): value is object {
-  const type = typeof value;
-  return value !== null && (type === 'object' || type === 'function');
-}
--- libraries/toolbelt/src/types/zod-validator.ts
+++
@@ -1,3 +0,0 @@
-import type { z } from 'zod';
-
-export type ZodValidator = z.ZodObject<z.ZodRawShape>;
