c89094fb14e71cfb38c0ef9dd8f1a91c53b25a3a
Ethan Glover <hello@ethang.email>
2024-02-15 12:12:17 -0600

    remove from monorepo

--- libraries/project-builder/src/util.ts
+++
@@ -1,9 +0,0 @@
-import { simpleGit } from 'simple-git';
-
-export async function getHasChanges(branch: string): Promise<boolean> {
-  const diffSummary = await simpleGit()
-    .fetch()
-    .diffSummary([`origin/${branch}`]);
-
-  return diffSummary.changed > 0;
-}
